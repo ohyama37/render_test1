e8a28dadfd6c797ef02dc2f21b93157bfa091e80
ohyama37 <111404853+ohyama37@users.noreply.github.com>
2024-03-15 01:19:39 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import List
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-class Data(BaseModel):
-    x: int
-    y: int
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get('/')
-async def index():
-    return {'message': 'Hello Data'}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.post('/')
-async def calc(data: Data):
-    z = data.x*data.y
-    return {'result': z}
+# @app1.get("/")
+# async def index():
+#     return {"message": "AAASuccess"}
+
+# Read
+@app.get("/users", response_model=List[schemas.User])
+async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+@app.get("/rooms", response_model=List[schemas.Room])
+async def read_rooms(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    rooms = crud.get_rooms(db, skip=skip, limit=limit)
+    return rooms
+
+@app.get("/bookings", response_model=List[schemas.Booking])
+async def read_bookings(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    bookings = crud.get_bookings(db, skip=skip, limit=limit)
+    return bookings
+
+# Create
+@app.post("/users", response_model=schemas.User)
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db=db, user=user)
+
+@app.post("/rooms", response_model=schemas.Room)
+async def create_room(room: schemas.RoomCreate, db: Session = Depends(get_db)):
+    return crud.create_room(db=db, room=room)
+
+@app.post("/bookings", response_model=schemas.Booking)
+async def create_booking(booking: schemas.BookingCreate, db: Session = Depends(get_db)):
+    return crud.create_booking(db=db, booking=booking)
